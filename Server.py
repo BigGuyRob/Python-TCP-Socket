05b16df17bbc994ff99550d849408394a8969b30
eckoo <68354392+eckoo@users.noreply.github.com>
2022-07-04 18:41:26 -0400

    final code

---
+++ Server.py
@@ -0,0 +1,27 @@
+import argparse
+
+from sys import argv
+import socket
+
+parser = argparse.ArgumentParser(description="""This is a very basic server program that reverses string data sent to it by client""")
+parser.add_argument('port', type=int, help='This is the port to listen for the client on', action='store')
+args = parser.parse_args(argv[1:])
+
+HOST = socket.gethostbyname(socket.gethostname())
+PORT = args.port
+
+MAX = 1024
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.bind((HOST,PORT))
+print(f'{HOST} listening on {PORT} :')
+sock.listen()
+conn,addr = sock.accept()
+print(f'{addr} has connected.')
+data = conn.recv(MAX)
+while(data):
+    answer = data.decode('utf-8')
+    conn.sendall(answer[::-1].encode('utf-8'))
+    data = conn.recv(MAX)
+sock.close()
+print('Program has completed.')
